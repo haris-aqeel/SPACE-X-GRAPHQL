f0f417d9633b75185820d7e2da07429deca388ae
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-10 13:44:45 +0500

    Making It A Progressive Web application

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ApolloProvider, InMemoryCache, ApolloClient  } from '@apollo/client';
+import registerServiceWorker from './registerServiceWorker';
+
 
 const client = new ApolloClient({
     uri: 'https://spacexdata.herokuapp.com/graphql',
@@ -12,3 +14,5 @@ ReactDOM.render(
     <ApolloProvider client={client}> <App /></ApolloProvider>
     ,document.getElementById('root')
     );
+
+    registerServiceWorker();
